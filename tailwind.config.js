8bc54fff97cb8c197040f6104a3565c640ba0675
Kaplya <kaplyak@protonmail.com>
2022-03-11 21:20:47 +0100

    Top bar width test commit.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'sans': ['ui-sans-serif', 'system-ui'],
+      'serif': ['ui-serif', 'Georgia'],
+      'mono': ['ui-monospace', 'SFMono-Regular'],
+      'display': ['Oswald'],
+      'body': ['Inter400'],
+    },
     extend: {
       colors: {
         'greybar': '#222',
